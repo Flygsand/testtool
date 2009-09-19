0f480c1c0eadfc9f32899370b6fd4188e0ba06c3
Bernhard R. Link <brlink@debian.org>
2009-09-19 16:15:14 +0000

    modernize build system a bit

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,6 @@ set -e
 
 if test -e .cvsignore ; then : ; else cp .cvsignore-suggested .cvsignore ; fi
 
-aclocal 
-autoheader
-automake -a -c
-autoconf
-./configure --enable-maintainer-mode CFLAGS="-Wall -g -O2 -Wmissing-prototypes -Wstrict-prototypes"
+autoreconf -i
+
+./configure --enable-maintainer-mode CFLAGS="-Wall -g -O2 -Wmissing-prototypes -Wstrict-prototypes -Wshadow"
